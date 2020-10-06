f4bbcf87f60996182d621304f29474aa5807957c
Cristiano Caon <cristiano.caon@hotmail.com>
2020-10-06 15:43:03 -0500

    Concepts

--- Materials/src/main/java/com/monotonic/collections/_2_what_are_collections/before/CollectionConcepts.java
+++ Materials/src/main/java/com/monotonic/collections/_2_what_are_collections/before/CollectionConcepts.java
@@ -4,8 +4,6 @@ import com.monotonic.collections.common.Product;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
 
 public class CollectionConcepts
 {
@@ -15,6 +13,46 @@ public class CollectionConcepts
         Product floorPanel = new Product("Floor Panel", 25);
         Product window = new Product("Glass Window", 10);
 
-        
+        Collection<Product> products = new ArrayList<>();
+        products.add(door);
+        products.add(floorPanel);
+        products.add(window);
+
+//        System.out.println(products);
+
+        // We can iterate through and update using an iterator,
+        // which is implemented with Collections.
+//        Iterator<Product> iterator = products.iterator();
+//        while(iterator.hasNext())
+//        {
+//            Product product = iterator.next();
+//            if(product.getWeight() > 20)
+//            {
+//                iterator.remove();
+//            }
+//        }
+//        System.out.println(products);
+
+        // Cannot iterate through the length of products and
+        // update (remove) items at the same times. At least not with
+        // for-each loop.
+//        for(Product product: products) {
+//            if(product.getWeight() > 20) {
+//                products.remove(product);
+//            }
+//            System.out.println(product);
+//        }
+
+        System.out.println(products.size());
+        System.out.println(products.isEmpty());
+        System.out.println(products.contains(window));
+//        System.out.println(products.remove(window));
+        System.out.println(products.contains(window));
+
+        Collection<Product> toRemove = new ArrayList<>();
+        toRemove.add(door);
+        toRemove.add(floorPanel);
+        products.removeAll(toRemove);
+        System.out.println(products);
     }
 }
